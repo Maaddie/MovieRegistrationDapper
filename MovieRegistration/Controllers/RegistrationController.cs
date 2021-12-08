d4d8d0d1e591add5239be879f76b14c0a7ec3ff4
Madalina Varasteanu <madalinavarasteanu@rocketmortgage.com>
2021-12-07 22:39:00 -0500

    Initial Commit

---
+++ MovieRegistration/Controllers/RegistrationController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRegistration.Models;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+
+
+namespace MovieRegistration.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = "select * from movies";
+
+                connect.Open();
+
+                List<Movie> movies = connect.Query<Movie>(sql).ToList();
+
+                connect.Close();
+                Movie m = movies[0];
+
+                return View(m);
+            }
+
+        }
+
+        public IActionResult Result(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                string sql = "INSERT INTO movies (ID, Title, Genre, Year, Runtime) Values (@ID, @Title, @Genre, @Year, @Runtime );";
+
+                using (var connection = new MySqlConnection(Secret.Connection))
+                {
+                    connection.Execute(sql, new { ID = m.ID, Title = m.Title, Genre = m.Genre, Year = m.Year, Runtime = m.Runtime });
+                }
+                return View(m);
+                //return RedirectToAction("Index", "Registration");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Registration");
+            }
+        }
+
+        public IActionResult viewMovies()
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = "select * from movies";
+
+                connect.Open();
+
+                List<Movie> movies = connect.Query<Movie>(sql).ToList();
+
+                connect.Close();
+                //Movie m = movies;
+
+                return View(movies);
+            }
+        }
+
+    }
+
+}
+
---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+        [Key]
+        public string ID { get; set; }
+        [MaxLength(30)]
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        [Range(1880,2021)]
+        public int Year { get; set; }
+
+        public float Runtime { get; set;}
+    }
+
+    //public enum Genre
+    //{
+    //    Action,
+    //    Animation,
+    //    Comedy,
+    //    Drama,
+    //    Romantic
+    //}
+}
